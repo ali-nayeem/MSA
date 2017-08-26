6fc27aa3c037cc04dee68775b5020cb5f42554a0
Nayeem <Nayeem@ACER-MAN>
2017-08-26 22:16:27 +0600

    Preparing classes for converting VAR to encodedVAR

---
+++ src/main/java/org/uma/jmetalmsa/stat/FindUniqueEncodedVAR.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.uma.jmetalmsa.stat;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.uma.jmetalmsa.problem.MSAProblem;
+import org.uma.jmetalmsa.problem.SATE_MSAProblem;
+import org.uma.jmetalmsa.score.Score;
+import org.uma.jmetalmsa.score.impl.EntropyScore;
+import org.uma.jmetalmsa.score.impl.GapConcentrationScore;
+import org.uma.jmetalmsa.score.impl.NumberOfAlignedColumnsScore;
+import org.uma.jmetalmsa.score.impl.SimilarityGapsScore;
+import org.uma.jmetalmsa.score.impl.SimilarityNonGapsScore;
+import org.uma.jmetalmsa.solution.MSASolution;
+import static org.uma.jmetalmsa.stat.CalculateObjetivesFromVAR.varFilePath;
+import static org.uma.jmetalmsa.stat.SearchAndEncodeVAR_Linux.instanceName;
+
+/**
+ *
+ * @author Nayeem
+ */
+public class FindUniqueEncodedVAR
+{
+
+    static int approxLineLength = 1200; 
+    static String instancePath = "dataset/100S";
+    static String instanceName = "R0";
+    static String encodedVarFileName = "/home/ali_nayeem/MSA/experiment/FinalCombinedEncodedVAR";
+    static String uniqueVarFileName = "/home/ali_nayeem/MSA/experiment/uniqueVAR";
+
+    public Set<String> getUniqueEncodedVarFile(String encodedVarFilePath, MSAProblem problem) throws Exception
+    {
+        int allCount = 0;
+        BufferedReader br = new BufferedReader(new FileReader(encodedVarFilePath));
+        Set<String> uniqueAlignments = new HashSet<>();
+
+        while (br.ready())
+        {
+            String oneLine = "";
+            StringBuilder encodedAlignment = new StringBuilder(problem.getNumberOfVariables() * approxLineLength);
+            encodedAlignment.append("<\n");
+
+            do
+            {
+                oneLine = br.readLine();
+            } while (oneLine != null && !oneLine.startsWith("<"));
+
+            while (oneLine != null)
+            {
+                oneLine = br.readLine();
+                if (oneLine.startsWith(">"))
+                {
+                    break;
+                }
+
+                encodedAlignment.append(oneLine).append("\n");
+
+            }
+
+            if (oneLine == null)
+            {
+                break;
+            }
+
+            encodedAlignment.append(">");
+            uniqueAlignments.add(encodedAlignment.toString());
+            allCount++;
+        }
+        br.close();
+        System.out.println("Total: "+allCount);
+        System.out.println("Unique:"+uniqueAlignments.size());
+        return uniqueAlignments;
+    }
+
+    public void printUniqueEncodedVarToFile(Set<String> allVar, String filepath) throws IOException
+    {
+        FileOutputStream stream = new FileOutputStream(filepath);
+        FileChannel channel = stream.getChannel();
+        byte[] strBytes;
+        ByteBuffer buffer;
+
+        for (String var : allVar)
+        {
+            strBytes = (var + "\n").getBytes();
+            buffer = ByteBuffer.allocate(strBytes.length);
+            buffer.put(strBytes);
+            buffer.flip();
+            channel.write(buffer);
+
+        }
+
+        stream.close();
+        channel.close();
+    }
+
+    public static void main(String[] arg) throws Exception
+    {
+        List<Score> scoreList = new ArrayList<>();
+        scoreList.add(new EntropyScore());
+        //scoreList.add(new NumberOfAlignedColumnsScore());
+        //scoreList.add(new NumberOfGapsScore());
+        //scoreList.add(new SimilarityGapsScore());
+        //scoreList.add(new SimilarityNonGapsScore());
+        //scoreList.add(new GapConcentrationScore());
+
+        MSAProblem problem = new SATE_MSAProblem(instanceName, instancePath, scoreList);
+        FindUniqueEncodedVAR selfObj = new FindUniqueEncodedVAR();
+        Set<String> uniqueVar = selfObj.getUniqueEncodedVarFile(encodedVarFileName, problem);
+        selfObj.printUniqueEncodedVarToFile(uniqueVar, uniqueVarFileName);
+        
+        //CalculateObjetivesFromVAR obj = new CalculateObjetivesFromVAR();
+        //ob.printPopulationToEncodedvarFile(pop, uniqueVarFileName);
+        //List <MSASolution> pop = obj.createPopulationFromEncodedVarFile(uniqueVarFileName, problem);
+        //obj.evaluatePopulationToFile(pop, "FUN." + problem.getName() + ".unique", 1, problem);
+
+    }
+
+}
